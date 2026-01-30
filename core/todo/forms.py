3d664b66f38507483ae324b0e64835eacdeb9ad6
Marzya <mnaq_1987@yahoo.com>
2026-01-30 08:40:00 +0330

    Initial commit

---
+++ core/todo/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control rounded-4",
+                "placeholder": "Enter Task Title"
+                  }
+        ),
+        label="title",
+    )
+
+    class Meta:
+        model = Task
+        fields = ['title']
---
+++ core/todo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'todo'
+urlpatterns = [
+    path('', views.TaskList.as_view(), name="task_list"),
+    path('task/create/',views.TaskCreate.as_view(), name="task_create"),
+    path('task/update/<int:pk>/', views.TaskUpdate.as_view(), name="task_update"),
+    path('task/complete/<int:pk>/', views.TaskComplete.as_view(), name="task_complete"),
+    path('task/delete/<int:pk>/',views.TaskDelete.as_view(), name="task_delete"),
+]
